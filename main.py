7034745d104ad11fbddb4f78d3ffbdc68c3ee3ec
putnaf <putnaf@gmail.com>
2025-01-22 21:02:45 -0500

    have player driving across screen

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
-import pygame
 from constants import *
+from player import *
+from circleshape import *
+import pygame
+
 
 def main():
     pygame.init()
     print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    fps = pygame.time.Clock()
+    dt = 0.03
+    player = Player(SCREEN_WIDTH/2, SCREEN_WIDTH/2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill("black")
+        player.update(dt)
+        player.draw(screen)
+        #dt = fps.tick() / 1000
+        fps.tick(60)
+        
         pygame.display.flip()
 
 
